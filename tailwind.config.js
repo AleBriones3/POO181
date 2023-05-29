185359a62ba1a4e57407c02986adef1a5910f44c
Ale <105374932+AleBriones3@users.noreply.github.com>
2023-05-29 16:01:22 -0600

    Interfaz (Búsqueda de Pacientes)
    Una interfaz muy básica de una búsqueda de pacientes, con ventanas emergentes y ejemplos no reales de los pacientes.

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+
+  plugins: [
+      require("flowbite/plugin")
+  ]
+
+}
+module.exports = {
+  content: [
+      "./templates/**/*.html",
+      "./static/src/**/*.js",
+      "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("flowbite/plugin")
+  ],
+}
+
