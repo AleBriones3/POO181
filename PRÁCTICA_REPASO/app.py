80d3d011a185ad4c29750df592749d116636a14a
Ale <105374932+AleBriones3@users.noreply.github.com>
2023-07-08 22:59:14 -0600

    Práctica de Repaso versión 1

---
+++ PRÁCTICA_REPASO/app.py
@@ -0,0 +1,123 @@
+""" Importación del Framework """
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+""" Importación de MySQL """
+from flask_mysqldb import MySQL
+
+""" Inicialización del APP """
+app= Flask(__name__)
+
+""" Configuración de la conexión """
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='db_Fruteria'
+app.secret_key='mysecretkey'
+mysql=MySQL(app)
+
+""" Declaración de la ruta, pertenece a http://localhost:5000  (El @app.route('/') es la ruta index o ruta principal)"""
+@app.route('/') 
+def index():
+    CC= mysql.connection.cursor()
+    CC.execute('select * from tbfrutas')
+    conFrutas= CC.fetchall()
+    print(conFrutas)
+    return render_template('index.html', listFrutas= conFrutas)
+
+""" Ruta http:localhost:5000/guardar tipo POST para Insert """
+""" "Methods" La ruta va a recibir información del formulario """
+@app.route('/buscar') 
+def BuscarF():
+    cursorId= mysql.connection.cursor()
+    cursorId.execute('SELECT * FROM tbfrutas where id=%s',(id,))
+    consultaId=cursorId.fetchone()
+    return render_template('BuscarFruta.html', frut=consultaId)
+
+@app.route('/buscarF', methods=['POST']) 
+def Buscar():
+    if request.method == 'POST':
+
+        """ Pasamos a variables el contenido de los imputs """
+        Vfruta= request.form['txtFruta']
+        Vtemporada= request.form['txtTemporada']
+        Vprecio= request.form['txtPrecio']
+        Vstock= request.form['txtStock']
+      
+        """ Conectar a la BD y ejecutar el insert """
+        CS= mysql.connection.cursor()
+        CS.execute('insert into tbfrutas(fruta, temporada, precio, stock) values(%s,%s,%s, %s)',(Vfruta,Vtemporada,Vprecio,Vstock))
+        mysql.connection.commit()
+        
+    flash('La fruta fue agregada correctamente')
+    return redirect(url_for('index'))
+
+@app.route('/ingresar') 
+def Ingresar():
+    cursorId= mysql.connection.cursor()
+    cursorId.execute('SELECT * FROM tbfrutas where id=%s',(id,))
+    consultaId=cursorId.fetchone()
+    return render_template('ingresarFruta.html', frut=consultaId)
+
+@app.route('/guardar', methods=['POST']) 
+def Guardar():
+    if request.method == 'POST':
+
+        """ Pasamos a variables el contenido de los imputs """
+        Vfruta= request.form['txtFruta']
+        Vtemporada= request.form['txtTemporada']
+        Vprecio= request.form['txtPrecio']
+        Vstock= request.form['txtStock']
+      
+        """ Conectar a la BD y ejecutar el insert """
+        CS= mysql.connection.cursor()
+        CS.execute('insert into tbfrutas(fruta, temporada, precio, stock) values(%s,%s,%s, %s)',(Vfruta,Vtemporada,Vprecio,Vstock))
+        mysql.connection.commit()
+        
+    flash('La fruta fue agregada correctamente')
+    return redirect(url_for('index'))
+
+@app.route('/editar/<id>') 
+def Editar(id):
+    cursorId= mysql.connection.cursor()
+    cursorId.execute('SELECT * FROM tbfrutas where id=%s',(id,))
+    consultaId=cursorId.fetchone()
+    return render_template('editarFruta.html', frut=consultaId)
+
+@app.route('/actualizar/<id>', methods=['POST']) 
+def Actualizar(id):
+    if request.method == 'POST':
+        Vfruta= request.form['txtFruta']
+        Vtemporada= request.form['txtTemporada']
+        Vprecio= request.form['txtPrecio']
+        Vstock= request.form['txtStock']
+
+        curAct= mysql.connection.cursor()
+        curAct.execute('UPDATE tbfrutas set fruta= %s, temporada= %s, precio=%s, stock=%s WHERE id=%s',(Vfruta, Vtemporada, Vprecio, Vstock , id))
+        mysql.connection.commit()
+
+    flash('Se actualizó la Fruta '+Vfruta)
+    return redirect(url_for('index'))
+
+@app.route('/borrar/<id>') 
+def Borrar(id):
+    CursorId= mysql.connection.cursor()
+    CursorId.execute('SELECT * FROM tbfrutas where id=%s',(id,))
+    ConsultaId=CursorId.fetchone()
+    return render_template('eliminarFruta.html', frut=ConsultaId)
+
+
+@app.route('/eliminar/<id>', methods=['POST']) 
+def Eliminar(id):
+    if request.method == 'POST':
+        Vfruta= request.form['txtFruta']
+
+        Cursorr= mysql.connection.cursor()
+        Cursorr.execute('DELETE FROM tbfrutas WHERE id=%s', (id,))
+        mysql.connection.commit()
+
+    flash('Se eliminó la Fruta '+Vfruta)
+    return redirect(url_for('index'))
+
+""" Ejecución del servidor a través del puerto 5000 """
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
